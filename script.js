4558f0c16a675c3499d925f490f9fed9e40fdb42
dd <121573466+LionHead858@users.noreply.github.com>
2025-12-28 14:53:45 +0100

    on klick open display

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 
 const dialogRef = document.getElementById("myDialog");
+ 
 
+
+// Öffnet den Dialog
 function openDialog() {
     dialogRef.showModal();
     dialogRef.classList.add("opened");
 }   
 
-
+// Schließt den Dialog
 function closeDialog() {
     dialogRef.close();
     dialogRef.classList.remove("opened");
@@ -15,22 +18,46 @@ function closeDialog() {
 
 
 
+let fruits = [1, 2, 3, 4, 5];
+
+function containsElement(array, element) {
+    return array.includes(element);
+
+}
+
+
+function initArrays(){
+    let contentRef = document.getElementById('my_content');
+    contentRef.innerHTML = "";
+
+    for (let indexFruits = 0; indexFruits < fruits.length; indexFruits++) {
+   
+   
+        contentRef.innerHTML += `<p>+ ${fruits[indexFruits]}</p>`;
+    
+      }
+    
+   }
+    
+
+
+
+// Liste
+
+
 // let Liste = [1, 2, 3, 4, 5];
 
-// let zahl = 10;
+
+// let displayElement = document.getElementById("main_Repeater");
+
+
+// let getElementById = Liste.getElementById("main_Repeater");
+
+// {
+//     displayElement.innerHTML = "<img src='./img/pictures/eins.jpg'></img>";
+// }
+
+
 
 // let mainElement = document.getElementById("ID_Main");
 // let displayElement = document.getElementById("ID_Display");
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(Liste);
