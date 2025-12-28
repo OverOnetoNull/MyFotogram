9639c76f983201fa999f5e7a1217de0163b4c37c
dd <121573466+LionHead858@users.noreply.github.com>
2025-12-28 13:04:00 +0100

    Dialog Öffnen Complete

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 
 
-let Liste = [1, 2, 3, 4, 5];
+const dialogRef = document.getElementById("myDialog");
 
-let zahl = 10;
-
-const mainElement = document.getElementById("ID_Main");
-const displayElement = document.getElementById("ID_Display");
-
-
-
-let editElement = document.("div");
+function openDialog() {
+    dialogRef.showModal();
+    dialogRef.classList.add("opened");
+}   
 
 
-count = Liste.push(zahl);
-
+function closeDialog() {
+    dialogRef.close();
+    dialogRef.classList.remove("opened");
+}
 
 
 
+// let Liste = [1, 2, 3, 4, 5];
+
+// let zahl = 10;
+
+// let mainElement = document.getElementById("ID_Main");
+// let displayElement = document.getElementById("ID_Display");
 
 
 
 
-console.log(Liste);
+
+
+
+
+
+
+
+
+// console.log(Liste);
