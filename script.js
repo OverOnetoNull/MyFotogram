f3d70fa19895a0591a49bd5150fa72bad6a8cfde
dd <121573466+LionHead858@users.noreply.github.com>
2025-12-28 12:14:55 +0100

    First Commit Pictures and HTML Scrpt Style

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+let Liste = [1, 2, 3, 4, 5];
+
+let zahl = 10;
+
+const mainElement = document.getElementById("ID_Main");
+const displayElement = document.getElementById("ID_Display");
+
+
+
+let editElement = document.("div");
+
+
+count = Liste.push(zahl);
+
+
+
+
+
+
+
+
+console.log(Liste);
